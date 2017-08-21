37cdb037a47ee15dd292653869f04f7f2ffbd379
simon.sim@pathmazing.com <chanvannara1993>
2017-08-21 16:34:31 +0700

    update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://simon:123456@ds023463.mlab.com:23463/simon-todos', {
     useMongoClient: true
 });
-
+ 
 module.exports = {
     mongoose
 }
