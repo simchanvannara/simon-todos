89d028ad2e238adb176296f6532b01858bd86796
simon.sim@pathmazing.com <chanvannara1993>
2017-08-21 16:33:53 +0700

    update

---
+++ server/db/mongoose.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://simon:123456@ds023463.mlab.com:23463/simon-todos', {
+    useMongoClient: true
+});
+
+module.exports = {
+    mongoose
+}
